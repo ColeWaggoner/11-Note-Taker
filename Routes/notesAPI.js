9fb59fb6efadf855e67670b129637972138a026a
Cole Waggoner <waggcj02@gmail.com>
2023-02-01 20:57:15 -0500

    Organized and got files ready for main code

---
+++ Routes/notesAPI.js
@@ -0,0 +1,8 @@
+const router = require("express").Router();
+const fs = require("fs");
+// For random ID
+const uuid = require("uuid");
+
+
+
+module.exports = router;
---
+++ server.js
@@ -0,0 +1,20 @@
+const express = require("express");
+const api = require("./routes/notesAPI");
+const html = require("./routes/notesHTML");
+
+const PORT = process.env.PORT || 3001;
+
+const app = express();
+
+// Middleware 
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+
+app.use(express.static("public"));
+
+// Connect to the other needed js
+app.use(api);
+app.use(html);
+
+app.listen(PORT, () => console.log(`Your server is running on port ${PORT}`));
