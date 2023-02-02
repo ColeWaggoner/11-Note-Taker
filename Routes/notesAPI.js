0fde08620d5cc2b852a7675781bfa9cd80084815
Cole Waggoner <waggcj02@gmail.com>
2023-02-01 21:10:41 -0500

    Got get request to work in Postman

--- Routes/notesAPI.js
+++ Routes/notesAPI.js
@@ -3,6 +3,15 @@ const fs = require("fs");
 // For random ID
 const uuid = require("uuid");
 
+// Sends db.json for adding to HTML page list
+router.get("/api/notes", (req, res) =>
+  fs.readFile("./db/db.json", (err, results) => {
+    if (err) {
+      throw err;
+    }
+    res.send(results);
+  })
+);
 
 
 module.exports = router;
